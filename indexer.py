8c71c4df5d9fc43dc4621670eaf0cc55889184db
Charliebebe <charlietran96@gmail.com>
2018-03-01 03:06:32 -0800

    Changed BS4 parser and soup calls, added new HTML tag elements to consider, ignored non-webpages during parse, implemented fast Porter stemming, printed index dimensions. TODO: Setup MongoDB for querying

--- indexer.py
+++ indexer.py
@@ -1,9 +1,11 @@
-import json;
-import re;
+import json
+import re
 from bs4 import BeautifulSoup
+import snowballstemmer
 from collections import Counter
 import math
-#Dictionary where keys = indexes and values = tokens
+
+# Dictionary where keys = indexes and values = tokens
 forwardindex = {}
 
 file = open("WEBPAGES_RAW/bookkeeping.json", "r")
@@ -11,53 +13,48 @@ file = open("WEBPAGES_RAW/bookkeeping.json", "r")
 #Dictionary that holds json formated data from file
 book = json.load(file)
 
-#List of indexes after performing split
-indexes = []
-
-#Go through each key in dictionary book and split it on the "/" then append to indexes List
-# for i in book.keys():
-#     #Splits key into a list of [directory, filename] then appends it to indexes
-#     x = i.split("/")
-#     indexes.append(x)
-#
-# count = 0
-
-#For every [directory,filename] in indexes
-# while (count < len(indexes)):
-#     # Create a deeper path
-#     path = "WEBPAGES_RAW/" + indexes[count][0] + "/" + indexes[count][1]
-#     newfile = open(path, encoding="utf8")
-#     readable = newfile.read()
-#     tokens = re.findall(r'[0-9a-z]+', readable)
-#     forwardindex[indexes[count]] = tokens
-#
-#     count = count + 1
+stemmer = snowballstemmer.stemmer('english')
 
 # For every [directory, filename] pair in book.keys()
 for localpath in book.keys():
     path = "WEBPAGES_RAW/" + localpath
     newfile = open(path, encoding="utf8")   # Open path
     readable = newfile.read().lower()
-    tokens = re.findall(r'[0-9a-z]+', readable)  # List of parsed tokens
-    if '<html>' not in readable:
+#    tokens = re.findall(r'[0-9a-z]+', readable)  # List of parsed tokens
+    if '<html>' in readable:    # Ignore non HTML files
+        soup = BeautifulSoup(readable, 'html.parser')
         forwardindex[path] = {}
-        forwardindex[path]['p'] = tokens
-        forwardindex[path]['h1'] = ''
-        forwardindex[path]['h2'] = ''
-        forwardindex[path]['h3'] = ''
-        forwardindex[path]['h4'] = ''
-        forwardindex[path]['h5'] = ''
-        forwardindex[path]['h6'] = ''
-    else:
-        soup = BeautifulSoup(readable, 'lmxl')
-        forwardindex[path] = {}
-        forwardindex[path]['p'] = re.findall('[0-9a-z]+', soup.find('p').text)
-        forwardindex[path]['h1'] = re.findall('[0-9a-z]+', soup.find('h1').text)
-        forwardindex[path]['h2'] = re.findall('[0-9a-z]+', soup.find('h2').text)
-        forwardindex[path]['h3'] = re.findall('[0-9a-z]+', soup.find('h3').text)
-        forwardindex[path]['h4'] = re.findall('[0-9a-z]+', soup.find('h4').text)
-        forwardindex[path]['h5'] = re.findall('[0-9a-z]+', soup.find('h5').text)
-        forwardindex[path]['h6'] = re.findall('[0-9a-z]+', soup.find('h6').text)
+        forwardindex[path]['normal'] = []
+        forwardindex[path]['h1'] = []
+        forwardindex[path]['h2'] = []
+        forwardindex[path]['h3'] = []
+        forwardindex[path]['h4'] = []
+        forwardindex[path]['h5'] = []
+        forwardindex[path]['h6'] = []
+        forwardindex[path]['title'] = []
+
+        for text in [el.text for el in soup.find_all('p')]:
+            forwardindex[path]['normal'].extend(stemmer.stemWords(re.findall('[0-9a-z]+', text)))
+        for text in [el.text for el in soup.find_all('span')]:
+            forwardindex[path]['normal'].extend(stemmer.stemWords(re.findall('[0-9a-z]+', text)))
+        for text in [el.text for el in soup.find_all('div')]:
+            forwardindex[path]['normal'].extend(stemmer.stemWords(re.findall('[0-9a-z]+', text)))
+
+        for text in [el.text for el in soup.find_all('h1')]:
+            forwardindex[path]['h1'].extend(stemmer.stemWords(re.findall('[0-9a-z]+', text)))
+        for text in [el.text for el in soup.find_all('h2')]:
+            forwardindex[path]['h2'].extend(stemmer.stemWords(re.findall('[0-9a-z]+', text)))
+        for text in [el.text for el in soup.find_all('h3')]:
+            forwardindex[path]['h3'].extend(stemmer.stemWords(re.findall('[0-9a-z]+', text)))
+        for text in [el.text for el in soup.find_all('h4')]:
+            forwardindex[path]['h4'].extend(stemmer.stemWords(re.findall('[0-9a-z]+', text)))
+        for text in [el.text for el in soup.find_all('h5')]:
+            forwardindex[path]['h5'].extend(stemmer.stemWords(re.findall('[0-9a-z]+', text)))
+        for text in [el.text for el in soup.find_all('h6')]:
+            forwardindex[path]['h6'].extend(stemmer.stemWords(re.findall('[0-9a-z]+', text)))
+        for text in [el.text for el in soup.find_all('title')]:
+            forwardindex[path]['title'].extend(stemmer.stemWords(re.findall('[0-9a-z]+', text)))
+
 
 inverted = {}
 
@@ -70,17 +67,18 @@ for path, termsdict in forwardindex.items():
 
                 # Set tag scaled tf
                 inverted[term][path] = \
-                    forwardindex[path]['p'].count(term) + \
+                    forwardindex[path]['normal'].count(term) + \
                     forwardindex[path]['h1'].count(term)*2 + \
-                    forwardindex[path]['h2'].count(term)*1.9 + \
-                    forwardindex[path]['h3'].count(term)*1.8 + \
-                    forwardindex[path]['h4'].count(term)*1.7 + \
-                    forwardindex[path]['h5'].count(term)*1.6 + \
-                    forwardindex[path]['h6'].count(term)*1.5
+                    forwardindex[path]['h2'].count(term)*1.7 + \
+                    forwardindex[path]['h3'].count(term)*1.6 + \
+                    forwardindex[path]['h4'].count(term)*1.5 + \
+                    forwardindex[path]['h5'].count(term)*1.4 + \
+                    forwardindex[path]['h6'].count(term)*1.3 + \
+                    forwardindex[path]['title'].count(term)*2
 
-                inverted[term][path] = 1 + math.log10(inverted[term][path])
+                inverted[term][path] = 1 + math.log10(inverted[term][path]) # Log tf
 
-
+# Idf
 for terms, docdict in inverted.items():
     for doc in docdict.keys():
         inverted[terms][doc] *= math.log10(len(forwardindex.keys() / len(docdict.keys())))
@@ -92,3 +90,7 @@ with open("invertedIndex.txt", "w") as f:
         for doc, score in val.items:
             f.write('{{ doc:{} score:{} }}, '.format(doc, score))
         f.write('\n')
+
+print 'Written to file invertedIndex.txt\n'
+print 'Number of documents: {}'.format(len(forwardindex.keys()))
+print 'Number of unique words (stemmed): {}'.format(len(inverted.keys()))
