5b72fe41f29e08571dd1c3c3248806129757f092
aguarda2 <aguarda2@users.noreply.github.com>
2018-03-08 20:04:55 -0800

    latest_changes

--- indexer.py
+++ indexer.py
@@ -2,19 +2,81 @@ import json
 import re
 from bs4 import BeautifulSoup
 import snowballstemmer
+import nltk
 from collections import Counter
 import math
 import io
+import string
+from sklearn.feature_extraction.text import TfidfVectorizer
 # Dictionary where keys = indexes and values = tokens
-forwardindex = {}
+
 
 file = io.open("WEBPAGES_RAW/bookkeeping.json", "r")
 
+
+
 #Dictionary that holds json formated data from file
 book = json.load(file)
-
+corpus = {}
+count = 0
 stemmer = snowballstemmer.stemmer('english')
-i = 0
+
+
+for localpath, link in book.items():
+    path = "WEBPAGES_RAW/" + localpath
+    newfile = io.open(path, encoding="utf8")   # Open path
+    readable = newfile.read().lower()
+    if '</html>' in readable:
+        soup = BeautifulSoup(readable, 'html.parser')
+        # for el in soup.find_all('p'):
+        #     doc= ' '.join([doc, ' '.join(stemmer.stemWords(re.findall(r'[0-9a-z]+', el.text)))])
+
+        doc = ' '.join([' '.join(stemmer.stemWords(re.findall(r'[0-9a-z]+', el.text))) for el in soup.find_all('p')])
+        corpus[link] = doc
+    else:
+        corpus[link] = ''
+    if count == 49:
+       break
+
+    count = count + 1
+
+
+vectorizer = TfidfVectorizer(stop_words='english',min_df=1,lowercase=True, sublinear_tf=True)
+X = vectorizer.fit_transform(corpus.values())
+import numpy
+numpy.set_printoptions(threshold=numpy.nan)
+print(X.T)
+# print(vectorizer.get_feature_names())
+# print (corpus)
+feature_names = vectorizer.get_feature_names()
+
+
+
+# for col, termrow in enumerate(X.T):
+#     print (feature_names[col], ' - '),
+#     print (termrow.shape)
+#     for doc in termrow:
+#         print (doc),
+#     print ('\n')
+
+# str = ['univ of waterloo', 'machin']
+# print (str)
+# response = vectorizer.transform(str)
+# print (response)
+# print (feature_names[959], feature_names[932], feature_names[574])
+# for col in X.nonzero()[1]:
+#     print (feature_names[col], ' - ', X[0, col])
+# print (X.shape)
+# idf = vectorizer.idf_
+# print(vectorizer.get_feature_names(), idf)
+#
+#
+# idf = vectorizer.idf_
+# inverted_index = dict(zip(vectorizer.get_feature_names(), idf))
+# print (inverted_index)
+"""
+
+
 # For every [directory, filename] pair in book.keys()
 for localpath in book.keys():
     path = "WEBPAGES_RAW/" + localpath
@@ -22,7 +84,7 @@ for localpath in book.keys():
     readable = newfile.read().lower()
 #    tokens = re.findall(r'[0-9a-z]+', readable)  # List of parsed tokens
     if '<html>' in readable:    # Ignore non HTML files
-        soup = BeautifulSoup(readable, 'html.parser')
+        
         forwardindex[path] = {}
         forwardindex[path]['normal'] = []
         forwardindex[path]['h1'] = []
@@ -55,8 +117,6 @@ for localpath in book.keys():
         for el in soup.find_all('title'):
             forwardindex[path]['title'].extend(stemmer.stemWords(re.findall('[0-9a-z]+', el.text)))
 
-        print '{}-th iteration. Processed {}\n'.format(i, path)
-        i += 1
 
 inverted = {}
 
@@ -66,26 +126,40 @@ for path, termsdict in forwardindex.items():
             if term not in inverted:
                 inverted[term] = Counter()  # {term: {doc: tf-idf weighted by tags}
 
-            # Set tag scaled tf
-            inverted[term][path] += \
-                forwardindex[path]['normal'].count(term) + \
-                forwardindex[path]['h1'].count(term)*2 + \
-                forwardindex[path]['h2'].count(term)*1.7 + \
-                forwardindex[path]['h3'].count(term)*1.6 + \
-                forwardindex[path]['h4'].count(term)*1.5 + \
-                forwardindex[path]['h5'].count(term)*1.4 + \
-                forwardindex[path]['h6'].count(term)*1.3 + \
-                forwardindex[path]['title'].count(term)*2
+                # Set tag scaled tf
+                inverted[term][path] = \
+                    forwardindex[path]['normal'].count(term) + \
+                    forwardindex[path]['h1'].count(term)*2 + \
+                    forwardindex[path]['h2'].count(term)*1.7 + \
+                    forwardindex[path]['h3'].count(term)*1.6 + \
+                    forwardindex[path]['h4'].count(term)*1.5 + \
+                    forwardindex[path]['h5'].count(term)*1.4 + \
+                    forwardindex[path]['h6'].count(term)*1.3 + \
+                    forwardindex[path]['title'].count(term)*2
 
-            print '{}-th iteration: Processed {}\n'.format(i, term)
-            i += 1
+                # Log-tf, divided by total tf in that document
+                inverted[term][path] = 1 + math.log10(inverted[term][path] / \
+                                                      (len(forwardindex[path]['normal']) + \
+                                                       len(forwardindex[path]['h1']) + \
+                                                       len(forwardindex[path]['h2']) + \
+                                                       len(forwardindex[path]['h3']) + \
+                                                       len(forwardindex[path]['h4']) + \
+                                                       len(forwardindex[path]['h5']) + \
+                                                       len(forwardindex[path]['h6']) + \
+                                                       len(forwardindex[path]['title']))) # Log tf
 
 # Idf normalization
 for terms, docdict in inverted.items():
     for doc in docdict.keys():
-        # Log-tf, divided by total tf in that document
-        inverted[term][doc] = 1 + math.log10(inverted[term][doc])  # Log tf
         inverted[terms][doc] *= math.log10( len(forwardindex.keys()) / len(docdict.keys()) )
+
+
+# with open("invertedIndex.txt", "w") as f:
+#     for term, val in inverted.items():
+#         f.write('{} - '.format(term))
+#         for doc, score in val.items:
+#             f.write('{{ doc:{} score:{} }}, '.format(doc, score))
+#         f.write('\n')
 
 inverted_index = []
 
@@ -110,8 +184,9 @@ for term, docs in inverted.items():
     inverted_index.append(dict)
 
 with io.open('inverted_index.json', 'w', encoding='utf-8') as json_file:
-    json_file.write(unicode(json.dumps(inverted_index, ensure_ascii=False)))
+    json.dump(inverted_index, json_file, ensure_ascii=False)
 
 print 'Written to file inverted_index.json\n'
 print 'Number of documents: {}'.format(len(forwardindex.keys()))
 print 'Number of unique words (stemmed): {}'.format(len(inverted.keys()))
+"""
